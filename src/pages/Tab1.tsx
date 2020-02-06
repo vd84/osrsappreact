cfe80db66c190c9e832c391945bdda2945770594
Douglas <dogge20@hotmail.com>
2020-02-06 20:45:56 +0100

    dsadsa

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,53 +1,56 @@
 import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonInput,
+    IonPage,
+    IonTitle,
+    IonToolbar
 } from '@ionic/react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
 import React from 'react';
 import './Tab1.css';
 
+
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonCard className="welcome-card">
-          <img src="/assets/shapes.svg" alt="" />
-          <IonCardHeader>
-            <IonCardSubtitle>Login</IonCardSubtitle>
-            <IonCardTitle>Welcome to OsrsApp</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              You can log in on this page
-            </p>
-          </IonCardContent>
-        </IonCard>
+
+  let state;
 
 
-      <button>Login</button>
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Login</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonCard className="welcome-card">
+                    <IonCardHeader>
+                        <IonCardSubtitle>Login</IonCardSubtitle>
+                        <IonCardTitle>Welcome to OsrsApp</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                        <p>
+                            You can log in on this page
+                        </p>
+                    </IonCardContent>
+                </IonCard>
 
 
-      </IonContent>
-    </IonPage>
-  );
+                <IonInput value={state}>Please type your login on osrs</IonInput>
+
+
+                <button>Login</button>
+
+
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
